17426c119f2e8e9deaa811f275248d2431f71755
Brandon <bmiller3822@gmail.com>
2020-02-28 13:50:01 -0500

    Fixed master

--- core/views.py
+++ core/views.py
@@ -5,35 +5,38 @@ from .models import Book
 from .forms import BookForm
 
 # def index(request):
-# 	return HttpResponse ("This is just a text response, unrelated to the database.") 
+# 	return HttpResponse ("This is just a text response, unrelated to the database.")
+
 
 def books(request):
-	books = Book.objects.all()
-	return render(request, 'core/books.html', {'books':books})
+    books = Book.objects.all()
+    return render(request, 'core/books.html', {'books': books})
 
-def notes_detail(request, pk):
-    note = Book.objects.get(pk=pk)
-    return render(request, 'core/book_detail.html', {'book': book, "pk":pk})
+
+def books_detail(request, pk):
+    book = Book.objects.get(pk=pk)
+    return render(request, 'core/book_detail.html', {'book': book, "pk": pk})
+
 
 def new_book(request):
-		if request.method == "POST":
-			form = BookForm(request.POST)
-			if form.is_valid():
-				book = form.save()
-				return redirect('books')
-		else:
-			form = BookForm()
-
-		return render(request, 'core/new_book.html', {'form': form})
-
-def edit_book(request, pk):
- note = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)
-        if form.is_valid(): 
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
             book = form.save()
-            return redirect('books-detail', pk=book.pk)
-    else: 
-        form = BookForm(instance=book)
+            return redirect('books')
+    else:
+        form = BookForm()
+
     return render(request, 'core/new_book.html', {'form': form})
 
+
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            book = form.save()
+            return redirect('books-detail', pk=book.pk)
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'core/new_book.html', {'form': form})
