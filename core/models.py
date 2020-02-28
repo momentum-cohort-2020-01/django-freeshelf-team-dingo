d532a688eebc3eee0b277d3373720ee6d403388f
Kat <kmneil2@gmail.com>
2020-02-28 17:21:20 -0500

    add created and updated time to book

--- core/models.py
+++ core/models.py
@@ -6,7 +6,8 @@ class Book(models.Model):
     author = models.CharField(max_length=200)
     description = models.TextField()
     url = models.CharField(max_length=500)
-    
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'Book title: {self.title} Author: {self.author} Description: {self.description} URL: {self.url}'
