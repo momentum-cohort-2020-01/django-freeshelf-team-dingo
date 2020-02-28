80ec43a7fddb8f0b75073c525bc96a1f18c37fe1
Kat <kmneil2@gmail.com>
2020-02-28 13:08:37 -0500

    edit and details views/html, categories html

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Book
@@ -11,6 +11,10 @@ def books(request):
 	books = Book.objects.all()
 	return render(request, 'core/books.html', {'books':books})
 
+def notes_detail(request, pk):
+    note = Book.objects.get(pk=pk)
+    return render(request, 'core/book_detail.html', {'book': book, "pk":pk})
+
 def new_book(request):
 		if request.method == "POST":
 			form = BookForm(request.POST)
@@ -21,4 +25,15 @@ def new_book(request):
 			form = BookForm()
 
 		return render(request, 'core/new_book.html', {'form': form})
+
+def edit_book(request, pk)
+ note = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid(): 
+            note = form.save()
+            return redirect('books-detail', pk=book.pk)
+    else: 
+        form = BookForm(instance=note)
+    return render(request, 'core/new_book.html', {'form': form})
 
--- freeshelf/urls.py
+++ freeshelf/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', views.books, name = 'books'),
     path('books/new/', views.new_book, name='new-book'),
+    path('books/<int:pk>', views.books_detail, name="books-detail"),
+    path('books/edit/<int:pk>', views.edit_book, name='edit-book'),
     # path('', views.index, name = 'index'),
 ]
